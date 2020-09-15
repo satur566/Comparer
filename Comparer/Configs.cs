7d842be6e8bd25ba0262e093b89eee321680903c
satur566 <satur566@gmail.com>
2020-09-15 22:32:48 +0300

    Some job is done.

---
+++ Comparer/Configs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparer
+{
+    static class Configs
+    {
+        private static string resultPath;
+        private static string referencePath;
+        private static int startIndex = 0;
+        private static int endIndex = 0;
+        private static int[] ignoreIndex;
+        public static string ResultPath 
+        {
+            get
+            {
+                return resultPath;
+            }
+            set
+            {
+                CheckFileAvailability(value); //TODO: check this!
+                resultPath = value;
+            }
+        }
+        public static string ResultEncoding { get; set; } //TODO: descript what encoding we get
+        public static string ReferencePath {
+            get
+            {
+                return referencePath;
+            }
+            set
+            {
+                CheckFileAvailability(value);
+                referencePath = value;
+            }
+        } //TODO: some tryCatch
+        public static string ReferenceEncoding { get; set; } //TODO: descript what encoding we get
+        public static int StartIndex
+        {
+            get
+            {
+                return startIndex;
+            }
+            set
+            {
+                if (value.GetType() != typeof(int))
+                {
+                    throw new Exception("Номер строки должен быть числом.");
+                }
+                startIndex = --value;
+            }
+        }
+        public static int EndIndex 
+        {
+            get
+            {
+                return endIndex;
+            }
+            set
+            {
+                if (value.GetType() != typeof(int))
+                {
+                    throw new Exception("Номер строки должен быть числом.");
+                }
+                endIndex = --value;
+            }
+        }
+        public static int[] IgnoreIndexes { get; set; } //TODO: -1 to set
+        private static bool IsFileLocked(string filename)
+        {
+            bool Locked = false;
+            try
+            {
+                FileStream fs =
+                    File.Open(filename, FileMode.OpenOrCreate,
+                    FileAccess.ReadWrite, FileShare.None);
+                fs.Close();
+            }
+            catch
+            {
+                Locked = true;
+            }
+            return Locked;
+        }
+        private static void CheckFileAvailability(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new Exception($"Файл {path} не сушествует.");
+            }
+            else if (IsFileLocked(path))
+            {
+                throw new Exception($"Файл {path} занят другим процессом.");
+            }
+        }
+    }
+}
--- Comparer/Program.cs
+++ Comparer/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace Comparer
 {
@@ -10,6 +13,115 @@ namespace Comparer
     {
         static void Main(string[] args)
         {
+            try
+            {
+                if (args.Length > 0)
+                {
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        switch (args[i])
+                        {
+                            case "-result":
+                                ExceptionMaker(ref args, ref i);
+                                Configs.ResultPath = args[++i];
+                                if (++i < args.Length &&
+                                    !args[i].StartsWith("-"))
+                                    {
+                                        Configs.ResultEncoding = args[i];
+                                    }
+                                break;
+                            case "-reference":
+                                ExceptionMaker(ref args, ref i);
+                                Configs.ReferencePath = args[++i];
+                                if (++i < args.Length &&
+                                    !args[i].StartsWith("-"))
+                                {
+                                    Configs.ReferenceEncoding = args[i];
+                                }
+                                break;
+                            case "-init":
+                                ExceptionMaker(ref args, ref i);
+                                Configs.StartIndex = Convert.ToInt32(args[++i]);
+                                break;
+                            case "-end":
+                                ExceptionMaker(ref args, ref i);
+                                Configs.EndIndex = Convert.ToInt32(args[++i]);
+                                break;
+                            case "-ignore":
+                                //TODO: code it
+                                break;
+                            case "-help":
+                                ShowHelp();
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    if (args.Contains("-run"))
+                    {
+                        Stopwatch stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        BeginCompare();
+                        stopwatch.Stop();
+                        Console.WriteLine($"Потрачено времени (миллисекунд): {stopwatch.ElapsedMilliseconds}.");
+                    }
+                }
+                else
+                {
+                    ShowHelp();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+        private static void ShowHelp()
+        {
+            //TODO: code helpBox!
+        }
+
+        private static void BeginCompare()
+        {
+            //TODO: some code.
+            string[] resultArray = File.ReadAllLines(Configs.ResultPath);
+            string[] referenceArray = File.ReadAllLines(Configs.ReferencePath);
+            int compareLength = resultArray.Length > referenceArray.Length ? referenceArray.Length : resultArray.Length;
+            Configs.EndIndex = Configs.EndIndex != 0 && compareLength > Configs.EndIndex ? Configs.EndIndex : compareLength;
+            int discrepancyIndex = -1;
+            for (int i = Configs.StartIndex; i < Configs.EndIndex; i++)
+            {
+                if (!resultArray[i].Equals(referenceArray[i]))
+                {
+                    discrepancyIndex = i;
+                    break;
+                }
+            }
+            if (discrepancyIndex.Equals(-1))
+            {
+                Console.WriteLine("Содержимое файлов идентично.");
+                return;
+            }
+            Console.WriteLine($"Первое различие встретилось на {discrepancyIndex + 1} строке:\n" +
+                $"Эталон: \t{ referenceArray[discrepancyIndex]}\nРезультат: \t{ resultArray[discrepancyIndex]}");
+        }
+
+        private static void ExceptionMaker(ref string[] array, ref int index)
+        {
+            if (index + 1 >= array.Length)
+            {
+                throw new Exception($"Отсутствуют аргументы параметра {array[index]}.");
+            }
+            if (array[index].Equals("-init") ||
+                array[index].Equals("-end"))
+            {
+                if (array[++index].GetType() != typeof(int))
+                {
+                    throw new Exception("Номер строки должен быть числом.");
+                }
+            }
         }
     }
 }
