99f527b0e3c2db239c726142716d27111e2bee60
satur566 <satur566@gmail.com>
2020-09-17 17:58:41 +0300

    This is really last.

--- Comparer/Program.cs
+++ Comparer/Program.cs
@@ -63,7 +63,6 @@ namespace Comparer
                     Console.WriteLine($"Потрачено времени (миллисекунд) всего: {globalStopwatch.ElapsedMilliseconds}.");
                 }
             }
-            Console.ReadKey();
         }
         /// <summary>
         /// Выводит подскизку по использованию консольного приложения.
