a300bf91902e5daf1a876279396833ccca080bdb
satur566 <satur566@gmail.com>
2020-09-18 15:17:21 +0300

    Removed Console.ReadLine()

--- Comparer/Program.cs
+++ Comparer/Program.cs
@@ -66,7 +66,6 @@ namespace Comparer
                     Console.WriteLine($"Потрачено времени (миллисекунд) всего: {globalStopwatch.ElapsedMilliseconds}.");
                 }
             }
-            Console.ReadKey();
         }
         /// <summary>
         /// Выводит подскизку по использованию консольного приложения.
