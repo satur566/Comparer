ec916328a5ab3c2fb6b08d50043300a31deb5c0d
satur566 <satur566@gmail.com>
2020-09-21 15:58:17 +0300

    Removed GetEncoding method, snatched from stackOverflow. Reason: it returns ASCII in any case.

--- Comparer/Comparing.cs
+++ Comparer/Comparing.cs
@@ -78,12 +78,12 @@ namespace Comparer
             }
             try
             {
-                using (StreamReader firstStream = new StreamReader(firstPath, GetEncoding(firstPath)))
-                using (StreamReader secondStream = new StreamReader(secondPath, GetEncoding(secondPath)))
+                using (StreamReader firstStream = new StreamReader(firstPath, Encoding.ASCII))
+                using (StreamReader secondStream = new StreamReader(secondPath, Encoding.ASCII))
                 {
                     for (int i = 0; i < EndIndex; i++)
                     {
-                        string resultString = firstStream.ReadLine();
+                        string resultString = firstStream.ReadLine();                        
                         string referenceString = secondStream.ReadLine();
                         if (i < StartIndex || IgnoreIndexes.Contains(i))
                         {
@@ -177,34 +177,6 @@ namespace Comparer
                 condition = maskedValue.Equals(unmaskedValue);
             }
             return condition;
-        }
-
-        /// <summary>
-        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
-        /// Defaults to ASCII when detection of the text file's endianness fails.
-        /// </summary>
-        /// <param name="filename">The text file to analyze.</param>
-        /// <returns>The detected encoding.</returns>
-        private Encoding GetEncoding(string filename) //Used this: https://stackoverflow.com/questions/3825390/effective-way-to-find-any-files-encoding
-        {
-            // Read the BOM
-            var bom = new byte[4];
-            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                file.Read(bom, 0, 4);
-            }
-
-            // Analyze the BOM
-            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
-            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
-            if (bom[0] == 0xff && bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0) return Encoding.UTF32; //UTF-32LE
-            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
-            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
-            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return new UTF32Encoding(true, true);  //UTF-32BE
-
-            // We actually have no idea what the encoding is if we reach this point, so
-            // you may wish to return null instead of defaulting to ASCII
-            return Encoding.ASCII;
         }
     }
 }
